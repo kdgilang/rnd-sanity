73dd9c41ec0ded028fe50221d83c432169e0b8d4
gilang <kadekgilangputra@gmail.com>
2023-06-17 22:55:29 +0800

    update build config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // reactStrictMode: true,
+  swcMinify: false,
   experimental: {
     appDir: true,
   },
@@ -9,7 +10,15 @@ const nextConfig = {
       'cdn.sanity.io',
       'scbufdfxcrgxyzojwvpv.supabase.co'
     ]
-  }
+  },
+  // typescript: {
+  //   // Set this to false if you want production builds to abort if there's type errors
+  //   ignoreBuildErrors: process.env.VERCEL_ENV === 'production',
+  // },
+  // eslint: {
+  //   /// Set this to false if you want production builds to abort if there's lint errors
+  //   ignoreDuringBuilds: process.env.VERCEL_ENV === 'production',
+  // },
 }
 
 module.exports = nextConfig
